a0148b04c1567e7a15d9b3af71e88c9e0d691a05
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-03-28 17:05:20 +0800

    modified main webpage

--- app.js
+++ app.js
@@ -7,17 +7,23 @@ const score = document.querySelector(".score");
 const scorePage = document.querySelector(".scorePage");
 const tube = document.querySelector(".tubeContainer");
 const gap = document.querySelector(".gap");
+const topTube = document.getElementById("top");
+const botTube = document.getElementById("bot");
 
 let moveUp = -50;
-let gravity = 5;
+let gravity = 6.5;
 let birdInitialTop = bird.offsetTop;
 let gameState = 0;
 
 function start() {
+  startGame.style.display = "none";
+  topTube.classList.add("anim");
+  botTube.classList.add("anim");
   gameState = 1;
   birdInitialTop += gravity;
   bird.style.top = birdInitialTop + "px";
   if (birdInitialTop <= 0 || birdInitialTop >= 570) {
+    gameContainer.removeChild(tube);
     gameState = 2;
     gravity = 0;
     birdInitialTop = bird.offsetTop;
@@ -29,7 +35,6 @@ function start() {
   }
   return gameState;
 }
-const gravityLoop = setInterval(start, 50);
 
 function moveBird() {
   let currentTop = bird.offsetTop;
@@ -37,7 +42,16 @@ function moveBird() {
   bird.style.top = currentTop + "px";
 }
 
+let scoreNum = 0;
+topTube.addEventListener("animationiteration", () => {
+  let randomNum = Math.random() * (400 - 100) + 100;
+  topTube.style.height = randomNum + "px";
+  scoreNum = scoreNum + 1;
+  score.innerText = `Score: ${scoreNum}`;
+});
+
 gameContainer.addEventListener("click", moveBird);
-tube.addEventListener("animationiteration", () => {
-  gap.style.top = 100 + "px";
+
+startGame.addEventListener("click", () => {
+  const gravityLoop = setInterval(start, 50);
 });
